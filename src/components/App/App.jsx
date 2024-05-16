df9ff227a89143508fa112ba5f06aadd288a8241
Anna Taranchuk <taranchuk4@yahoo.com>
2024-05-16 21:44:37 +0300

    step 3-4

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,14 +4,14 @@ import ContactForm from "../ContactForm/ContactForm";
 import ContactList from "../ContactList/ContactList";
 import SearchBox from "../SearchBox/SearchBox";
 
-export default function App() {
-  const initialContacts = [
-    { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-    { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-    { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-    { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-  ];
+const initialContacts = [
+  { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+  { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+  { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+  { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+];
 
+export default function App() {
   const [contacts, setContacts] = useState(initialContacts);
   const [filter, setFilter] = useState("");
 
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,8 +1,19 @@
-import { Formik, Form, Field } from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import { useId } from "react";
 import { nanoid } from "nanoid";
 import css from "./ContactForm.module.css";
+
+const ContactSchema = Yup.object().shape({
+  name: Yup.string()
+    .min(3, "Too short!")
+    .max(50, "Too short!")
+    .required("This field must be filled in!"),
+  number: Yup.string()
+    .min(3, "Too short!")
+    .max(50, "Too long!")
+    .required("This field must be filled in!"),
+});
 
 export default function ContactForm({ onAdd }) {
   const initialValues = { name: "", number: "" };
@@ -14,16 +25,22 @@ export default function ContactForm({ onAdd }) {
   };
 
   return (
-    <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+    <Formik
+      initialValues={initialValues}
+      validationSchema={ContactSchema}
+      onSubmit={handleSubmit}
+    >
       <Form className={css.form}>
         <div className={css.formGroup}>
           <label htmlFor={`${fieldId}-name`}>Name</label>
-          <Field name="name" id={`${fieldId}-name`} />
+          <Field className={css.input} name="name" id={`${fieldId}-name`} />
+          <ErrorMessage className={css.error} name="name" component="span" />
         </div>
 
         <div className={css.formGroup}>
           <label htmlFor={`${fieldId}-number`}>Number</label>
-          <Field name="number" id={`${fieldId}-number`} />
+          <Field className={css.input} name="number" id={`${fieldId}-number`} />
+          <ErrorMessage className={css.error} name="number" component="span" />
         </div>
 
         <button type="submit" className={css.btn}>
